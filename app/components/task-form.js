d554babcf0c98a1e159c890539836ade081bd32e
Kervyn <73232587+kervyntan@users.noreply.github.com>
2023-12-21 23:32:03 +0800

    added validation

--- app/components/task-form.js
+++ app/components/task-form.js
@@ -10,35 +10,70 @@ export default class TaskFormComponent extends Component {
     status: '',
     dueDate: null,
   };
-  @tracked hasSubmitted = false;
+
+  @tracked validity = {
+    title: false,
+    description: false,
+    status: false,
+    dueDate: false,
+  };
+
+  @tracked updatedSuccessful = false;
+
+  @tracked areInputsValid = this.checkValidityInputs();
 
   @action
-  updateTaskModel() {
-    // try {
-    //   await fetch('http://localhost:2000/api/Tasks', {
-    //     method: 'POST',
-    //     body: JSON.stringify(this.userInput),
-    //   });
-
-    //   console.log('Updated Task model successfully');
-    // } catch (e) {
-    //   console.log(e);
-    // }
-
-    fetch('http://localhost:2000/api/Tasks', {
-      method: 'POST',
-      body: this.userInput,
-    })
-      .then((res) => {
-        console.log(res.json());
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+  preventDefault(e) {
+    e.preventDefault();
   }
 
   @action
-  logUserInputs() {
-    console.log(this.userInput);
+  checkValidityInputs() {
+    for (let prop in this.validity) {
+      if (this.validity[prop] == false) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  @action
+  checkUserInputs() {
+    for (let input in this.userInput) {
+      if (input != 'dueDate' && this.userInput[input].length > 0) {
+        // console.log(input)
+        this.validity[input] = true;
+      }
+
+      if (input == 'dueDate') {
+        if (new Date(this.userInput[input]) >= new Date()) {
+          this.validity[input] = true;
+        }
+      }
+    }
+  }
+
+  @action
+  updateTaskModel() {
+    if (this.checkValidityInputs()) {
+      fetch('http://localhost:2000/api/Tasks', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(this.userInput),
+      })
+        .then((res) => {
+          console.log(res.json());
+          console.log('Updated Task model successfully');
+          this.updatedSuccessful = true;
+          setTimeout(() => {
+            this.updatedSuccessful = false;
+          }, 100000);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
   }
 }
