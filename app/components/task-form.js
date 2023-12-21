b762e289c058d6c9a3b60c1656de82723199e2ac
Kervyn <73232587+kervyntan@users.noreply.github.com>
2023-12-21 16:59:16 +0800

    added event listeners

--- app/components/task-form.js
+++ app/components/task-form.js
@@ -1,6 +1,7 @@
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
+import fetch from 'fetch';
 
 export default class TaskFormComponent extends Component {
   @tracked userInput = {
@@ -13,7 +14,27 @@ export default class TaskFormComponent extends Component {
 
   @action
   updateTaskModel() {
-    console.log('Updated Task model successfully');
+    // try {
+    //   await fetch('http://localhost:2000/api/Tasks', {
+    //     method: 'POST',
+    //     body: JSON.stringify(this.userInput),
+    //   });
+
+    //   console.log('Updated Task model successfully');
+    // } catch (e) {
+    //   console.log(e);
+    // }
+
+    fetch('http://localhost:2000/api/Tasks', {
+      method: 'POST',
+      body: this.userInput,
+    })
+      .then((res) => {
+        console.log(res.json());
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }
 
   @action
